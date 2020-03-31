32436bfef30bcf4b5b04872c009a90b096e99e28
michel.remillard <michel.remillard@farmersedge.ca>
2020-03-30 19:59:58 -0500

    finally found the bug

--- audit.rb
+++ audit.rb
@@ -50,15 +50,15 @@ slack_notify('Set Gcloud project failed.', slack_k8s_snapshotter_app_webhook.to_
 context_check = `#{set_context} > /dev/null 2>&1 ; echo $?`
 slack_notify('Could not set kube context.', slack_k8s_snapshotter_app_webhook.to_s) unless context_check.to_i.zero?
 
-# Get all of the physical volumes for the current context.
-pv_flat_list = `kubectl get pv -o name | grep -v local`
+# Get all of the physical volumes for the current context.  We only want the "Bound" volumes, not the "Available, Released, Terminating, etc." volumes.
+pv_flat_list = `kubectl get pv -o=jsonpath="{.items[?(@.status.phase=='Bound')]['.metadata.name']}"`
 pv_arr = pv_flat_list.split(' ')
 pv_report_arr = []
 
 # For each PV, check to see if an annotation for the snapshotter needs to be added.
 pv_arr.each do |pv|
   puts "Checking #{pv} for annotation."
-  pv_deleted = `kubectl describe #{pv} | grep backup.kubernetes.io.deltas > /dev/null 2>&1`
+  pv_deleted = `kubectl describe persistentvolume #{pv} | grep backup.kubernetes.io.deltas > /dev/null 2>&1`
   if pv_deleted[/(Not Found)/]
     # As the main loop can take a while to complete, just ensure the PV has not been deleted in the mean while.
     # Example: Error from server (NotFound): persistentvolumes "pvc-dbf41f81-2e44-11ea-b136-4201ac100008" not found
@@ -66,21 +66,21 @@ pv_arr.each do |pv|
     next
   end
 
-  claim_line = `kubectl describe #{pv} | grep Claim: `
+  claim_line = `kubectl describe persistentvolume #{pv} | grep Claim: `
   claim_line_arr = claim_line.match(%r{^Claim:\s+(?<claim_name>[a-z0-9-]+)\/[a-z0-9-]+$})
-  delta_check = `kubectl describe #{pv} | grep backup.kubernetes.io.deltas > /dev/null 2>&1 ; echo $?`
+  delta_check = `kubectl describe persistentvolume #{pv} | grep backup.kubernetes.io.deltas > /dev/null 2>&1 ; echo $?`
   if delta_check.to_i.zero?
-    annotation = `kubectl describe #{pv} | grep backup.kubernetes.io.deltas`
+    annotation = `kubectl describe persistentvolume #{pv} | grep backup.kubernetes.io.deltas`
     backup_schedule = annotation[/P.*$/]
     pv_report_line_arr = [claim_line_arr[:claim_name], pv, "Schedule: #{backup_schedule}"]
   else
     # The k8s-snapshots program will consider any GKE PVC which lacks "region" or "zone" within the PV Labels as an "Unsupported Volume".
     # NFS and Rook are not supported, most likely they are missing the "gcePersistentDisk" (via "get -o yaml") which points to the actual disk.
     # "Zone" and "region" appear under "labels:" as part of the "failure-domain".
-    supported_volume = `kubectl describe #{pv} | grep failure-domain.beta.kubernetes.io > /dev/null 2>&1 ; echo $?`
+    supported_volume = `kubectl describe persistentvolume #{pv} | grep failure-domain.beta.kubernetes.io > /dev/null 2>&1 ; echo $?`
     if supported_volume.to_i.zero?
       puts "Adding annotation to this PV: #{pv}."
-      patch_ok = `kubectl patch #{pv} -p '{"metadata": {"annotations": {"backup.kubernetes.io/deltas": "P1D P14D"}}}' > /dev/null 2>&1 ; echo $?`
+      patch_ok = `kubectl patch persistentvolume #{pv} -p '{"metadata": {"annotations": {"backup.kubernetes.io/deltas": "P1D P14D"}}}' > /dev/null 2>&1 ; echo $?`
       slack_notify("Failed to patch #{pv}!", slack_k8s_snapshotter_app_webhook.to_s) unless patch_ok.to_i.zero?
       pv_report_line_arr = [claim_line_arr[:claim_name], pv, '{\color{blue}Added to Snapshotter Schedule}']
     else
@@ -89,6 +89,7 @@ pv_arr.each do |pv|
     end
   end
   pv_report_arr.push(pv_report_line_arr)
+  puts "Report Line: #{pv_report_line_arr[0]} #{pv_report_line_arr[1]} #{pv_report_line_arr[2]} REPORT SIZE: #{pv_report_arr.length}"
 end
 
 # Report Preamble
@@ -105,7 +106,14 @@ report.push('\maketitle')
 
 # Report Meat and Potatoes
 namespace = ''
+puts "REPORT SIZE before sort: #{pv_report_arr.length}"
 pv_report_arr.sort!
+
+puts "Pre print array #{pv_report_arr.length}"
+pv_report_arr.each do |line|
+  print "PRINT REP: #{line[0]},  #{line[1]}, #{line[2]} \n"
+end
+
 pv_report_arr.each do |line|
   if line[0] != namespace
     report.push('\end{itemize}') unless namespace == ''
@@ -114,8 +122,7 @@ pv_report_arr.each do |line|
     report.push('\begin{itemize}')
     puts "Preparing report for namespace: #{namespace}."
   end
-  persistent_volume = line[1].match(%r{persistentvolume\/(?<persistent_volume>.+)$})
-  report.push('\item PVC: ' + persistent_volume[:persistent_volume] + ' ' + line[2])
+  report.push('\item PVC: ' + line[1] + ' ' + line[2])
   # Pretty safe to assume any snapshot schedule will at least be done daily (thus "P1D"), so if we see that in the PV annotations,
   # we will check for the number of snapshots, as well as the date of the oldest and newest snapshots.
   next unless line[2].match?(/P1D/)
